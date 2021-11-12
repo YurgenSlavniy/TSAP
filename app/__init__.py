f165d63dd23199988133383be9c6ef4370f0054a
AnIv83 <andrey.iv.cherkessk@gmail.com>
2021-11-12 20:58:21 +0300

    Create starter app

---
+++ app/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from dotenv import load_dotenv
+
+# Controllers.
+from app.controller.home import bp as home
+
+#
+import os
+
+# Aвтоматичкская установка переменных окружения
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+controllers = [
+    home,
+]
+
+def create_app()-> "flask":
+    app = Flask(__name__)
+
+    for c in controllers:
+        app.register_blueprint(c)
+
+    return app
